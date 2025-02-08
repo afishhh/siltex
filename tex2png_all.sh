258e871c9a61166a08d78c774f02f6592d0a39e7
Hubert Głuchowski <fishhh@fishhh.dev>
2025-02-08 16:47:20 +0100

    Fix globbing

--- tex2png_all.sh
+++ tex2png_all.sh
@@ -5,9 +5,11 @@ if [[ $# -ne 2 ]]; then
   exit 1
 fi
 
+shopt -s globstar
+
 in="$1"
 out="$2"
-for f in "$in"/*/**.tex; do
+for f in "$in"/**/*.tex; do
   noprefix=${f##"$in"}
   mkdir -p "$(dirname "$out/$noprefix")"; ./target/release/siltex tex2png -o "$out/${noprefix%%.tex}.png" "$f" || echo "$f failed";
 done
